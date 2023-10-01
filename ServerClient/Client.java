c6b25e6b727534bf17b536baa2a82a779837ccfc
t4tomer <tomer.polsky27@gmail.com>
2023-10-01 11:27:52 +0300

    be4 changes in the Server&Client codes

--- ServerClient/Client.java
+++ ServerClient/Client.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
-        String serverAddress = "localhost"; // Change this to the server's address
-        int serverPort = 12345; // Change this to the server's port number
-        
+        String serverAddress = "127.0.0.1"; // IP address of the server
+        int serverPort = 80; // Port of the server
+
         try (Socket socket = new Socket(serverAddress, serverPort);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             Scanner scanner = new Scanner(System.in)) {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                Scanner scanner = new Scanner(System.in)) {
 
             System.out.println("Connected to server");
 
@@ -22,16 +19,16 @@ public class Client {
                 System.out.print("Enter a message (or 'exit' to quit): ");
                 String message = scanner.nextLine();
 
-                if ("exit".equalsIgnoreCase(message)) {
-                    break;
-                }
-
                 // Send the message to the server
                 out.println(message);
 
                 // Receive and display the server's response
                 String response = in.readLine();
                 System.out.println("Server response: " + response);
+
+                if ("exit".equalsIgnoreCase(message)) {
+                    break; // Exit the loop if the user sends 'exit'
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
--- ServerClient/Server.java
+++ ServerClient/Server.java
@@ -1,27 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
     public static void main(String[] args) {
-        int port = 12345; // Change this to the desired port number
-        
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server is listening on port " + port);
-            
+        int serverPort = 80; // Port of the server
+
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            System.out.println("Server is listening on port " + serverPort);
+
             while (true) {
-                try (Socket clientSocket = serverSocket.accept()) {
+                try (Socket clientSocket = serverSocket.accept();
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
                     System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                    // Create a BufferedReader to read messages from the client
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
                     String message;
-                    while ((message = reader.readLine()) != null) {
+                    while ((message = in.readLine()) != null) {
                         System.out.println("Received from client: " + message);
+
+                        // Check if the client wants to exit
+                        if ("exit".equalsIgnoreCase(message)) {
+                            serverSocket.close();
+                            // clientSocket.close();
+                            break; // Close the socket if "exit" is received
+                        }
+
+                        // Send a response to the client
+                        String response = "ACK!";
+                        out.println(response);
+
+                        System.out.println("Response sent to client");
                     }
 
                     System.out.println("Client disconnected: " + clientSocket.getInetAddress());
